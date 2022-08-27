95948272377c51dd305f6ac328eeee7fde079fbb
Kutas Ferenc <ferenc.kutas@gmail.com>
2022-08-27 12:13:38 +0200

    Semi working solution. Idex problems solved, results are incoorect so far

--- heimerdinger.py
+++ heimerdinger.py
@@ -1,3 +1,4 @@
+from cmath import nan
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,48 +9,59 @@ c = int(input())
 c_inc = c + 1
 usefulnessMatrix = []
 
+# Init an n*n matrix with 0-s
 for x in range(c_inc):
     row = []
     for y in range(c_inc):
-        row.append(0)
+        row.append(nan)
     
     usefulnessMatrix.append(row)
 
-# If the given point is on or above the main diagonal, 'h' is the 'y' coordinate of the point
+# Set usefulnesses on the main diagonal and below it (y coordinate)
+# Above the main diagonal everything stays 0
 for x in range(c_inc):
     for y in range(c_inc):
         if (c <= x + y):
             usefulnessMatrix[x][y] = y
-        elif( c - 1 <= x + y):
-            usefulnessMatrix[x][y] = c - 1
-        else:
-            usefulnessMatrix[x][y] = 0
 
+# Calculate usefulness above the main diagonal
+# NOTE: the following calculations are made by a formula created by Mate Szalai
+def calcUsefulness(x, y, p):
+    # Calculate the summa which needs to ba maximised later
+    sum = 0
+    for k in range(c - (x + y)):
+        coordX = 2 * (c - (x + y + k))
+        coordY = x + k
+
+        try:
+            usefulnessPart = usefulnessMatrix[coordX][coordY]
+
+            binomialPart = math.factorial(c - (x + y)) / (math.factorial(c - x - y - k) * math.factorial(k))
+            probabilityPart = math.pow(p, c - (x + y) - k) * math.pow(1 - p, k)
+
+            partSolution = usefulnessPart * binomialPart * probabilityPart
+
+            sum = sum + partSolution
+
+        except IndexError:
+            sum = y
+
+    return sum
+
+# Find the max usefulness for the given matrix element with the given p by 0.0001 precision
+samples = np.linspace(0, 1, 10000)
+for x in range(0, c):
+    for y in range(0, c - x):
+        maxVal = 0
+        for p in samples:
+            val = calcUsefulness(x, y, p)
+
+            if (val > maxVal):
+                maxVal = val
+
+        # Set the calculated max usefulness
+        usefulnessMatrix[x][y] = maxVal
+
+# Print matrix
 for i in range(c_inc):
     print(usefulnessMatrix[i])
-
-
-
-# def calcBinomial(n, k):
-#     return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
-
-# def maximise(probs):
-#     array = []
-#     for p in probs:
-#         sum = 0
-#         for i in range(max_i):
-#             # Calculate 'usefulness'
-#             h = calcUsefulness(2 * (c - (2 + max_i + i)), max_i + i)
-            
-            
-#             # Calculate further binomial and exponential components
-#             exponent = c - (2 + max_i) - i
-#             b = calcBinomial(max_i, i) * pow(p, exponent) * pow(1 - p, i)
-
-#             # Put the quation together
-#             sum = sum + h * b
-#             print(sum)
-        
-#         array.append(sum)
-
-#     return array
