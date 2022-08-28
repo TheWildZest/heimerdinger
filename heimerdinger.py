dd1a4083be769822ad330f6bdb624965b065e060
Kutas Ferenc <ferenc.kutas@gmail.com>
2022-08-28 11:47:44 +0200

    Save last stage

--- heimerdinger.py
+++ heimerdinger.py
@@ -1,6 +1,5 @@
 from cmath import nan
 import math
-import matplotlib.pyplot as plt
 import numpy as np
 
 print('Choose the treshold!')
@@ -8,6 +7,7 @@ print('Choose the treshold!')
 c = int(input())
 c_inc = c + 1
 usefulnessMatrix = []
+
 
 # Init an n*n matrix with 0-s
 for x in range(c_inc):
@@ -17,16 +17,21 @@ for x in range(c_inc):
     
     usefulnessMatrix.append(row)
 
+
 # Set usefulnesses on the main diagonal and below it (y coordinate)
 # Above the main diagonal everything stays 0
 for x in range(c_inc):
     for y in range(c_inc):
+        if (x == 0):
+            usefulnessMatrix[x][y] = y
+
         if (c <= x + y):
             usefulnessMatrix[x][y] = y
 
+
 # Calculate usefulness above the main diagonal
 # NOTE: the following calculations are made by a formula created by Mate Szalai
-def calcUsefulness(x, y, p):
+def calcUsefulness(x, y, j, i, p):
     # Calculate the summa which needs to ba maximised later
     sum = 0
     for k in range(c - (x + y)):
@@ -36,8 +41,8 @@ def calcUsefulness(x, y, p):
         try:
             usefulnessPart = usefulnessMatrix[coordX][coordY]
 
-            binomialPart = math.factorial(c - (x + y)) / (math.factorial(c - x - y - k) * math.factorial(k))
-            probabilityPart = math.pow(p, c - (x + y) - k) * math.pow(1 - p, k)
+            binomialPart = math.factorial(c - (j + i)) / (math.factorial(c - j - i - k) * math.factorial(k))
+            probabilityPart = math.pow(p, c - (j + i) - k) * math.pow(1 - p, k)
 
             partSolution = usefulnessPart * binomialPart * probabilityPart
 
@@ -48,19 +53,25 @@ def calcUsefulness(x, y, p):
 
     return sum
 
+
 # Find the max usefulness for the given matrix element with the given p by 0.0001 precision
 samples = np.linspace(0, 1, 10000)
-for x in range(0, c):
-    for y in range(0, c - x):
+for j in range(1, c - 1):
+    for i in range(0, c - j):
         maxVal = 0
+
+        coordX = c - (j + i)
+        coordY = i
+
         for p in samples:
-            val = calcUsefulness(x, y, p)
+            val = calcUsefulness(coordX, coordY, j, i, p)
 
             if (val > maxVal):
                 maxVal = val
 
         # Set the calculated max usefulness
-        usefulnessMatrix[x][y] = maxVal
+        usefulnessMatrix[j][i] = maxVal
+
 
 # Print matrix
 for i in range(c_inc):
